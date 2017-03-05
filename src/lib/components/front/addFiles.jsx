6533419191c4409dcd09e795dd3121b81717f6bd
narghev <n.ghevandiani@gmail.com>
2017-03-06 00:55:20 +0400

    add files input bug fix

--- src/lib/components/front/addFiles.jsx
+++ src/lib/components/front/addFiles.jsx
@@ -1,23 +1,9 @@
 import React from 'react';
 
-/*export default
-class AddFiles extends React.Component {
-  handleChange = (event) => {
-    this.props.filePassFunc(event.target.files);
-  }
-  render(){
-    return (
-      <div>
-        <input type="file" accept=".mp3, .wav" className="addFiles" id="add" onChange={this.handleChange} multiple/>
-        <label htmlFor="add"><img src="images/addFiles.png" /></label>
-      </div>
-    )
-  }
-}*/
-
 export const AddFiles = ({filePassFunc}) => {
   const handleChange = (event) => {
     filePassFunc(event.target.files);
+    event.target.value = "";
   }
   return (
     <div>
