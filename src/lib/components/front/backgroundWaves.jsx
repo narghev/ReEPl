5c0be29af983127c71aee45fcc31f4e2e38b5d7c
narghev <n.ghevandiani@gmail.com>
2017-03-19 01:00:31 +0400

    minor changes

--- src/lib/components/front/backgroundWaves.jsx
+++ src/lib/components/front/backgroundWaves.jsx
@@ -5,6 +5,8 @@ export const BackGroundWaves = () => {
     let animation = setInterval(()=>{
       if (window.animationNumber != 3){
         clearInterval(animation);
+        clearInterval(ctxClearInterval);
+        clearInterval(depthChangeInterval);
         return;
       }
       let frequencyNow = getAverageFrequency();
--- src/scripts/waves.js
+++ src/scripts/waves.js
@@ -2,6 +2,7 @@ let canvas;
 let ctx;
 let canvasW;
 let canvasH;
+let depthDirection = 1;
 
 const initCanvas = () => {
   if (canvas !== undefined)
@@ -13,17 +14,19 @@ const initCanvas = () => {
   canvasW = canvas.width;
   ctxClearInterval = setInterval(()=>{
     ctx.clearRect(0,0,canvasW, canvasH);
-  },50);
+  },75);
+  depthChangeInterval = setInterval(()=>{
+    depthDirection *= -1;
+  },15000);
 }
 
 const drawOnCanvas = (depth) => {
   initCanvas();
-  depth *= 10;
-  console.log(depth)
+  depth *= 5*depthDirection;
   ctx.beginPath();
   ctx.moveTo(0, canvasH/2);
   ctx.bezierCurveTo(canvasW/8, (canvasH/2)-depth, 3*canvasW/8, (canvasH/2)+depth, 4*canvasW/8, canvasH/2);
-  ctx.moveTo(canvasW/2, canvasH/2);
   ctx.bezierCurveTo(5*canvasW/8, (canvasH/2)-depth, 7*canvasW/8, (canvasH/2)+depth, canvasW, canvasH/2);
   ctx.stroke();
+  ctx.closePath();
 }
