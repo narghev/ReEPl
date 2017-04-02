af1185a51492c67f78f3ceb62e197345616c093c
narghev <n.ghevandiani@gmail.com>
2017-04-02 14:25:58 +0400

    menu button redesign

--- src/lib/components/front/playlistButton.jsx
+++ src/lib/components/front/playlistButton.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Menu from 'material-ui/svg-icons/navigation/apps';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export const PlaylistButton = ({clickHandler}) => {
   return(
-    <div className="playlistButton">
-      <img src="images/playlist.png" onClick={ clickHandler }/>
+    <div className="playlistButton" onClick={ clickHandler }>
+      <MuiThemeProvider>
+        <Menu color="#FFFDE7" style={{width: '35px', height: '35px'}} />
+      </MuiThemeProvider>
     </div>
   )
 }
