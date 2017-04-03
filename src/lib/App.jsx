8a286b6fcbbbef7bd8eca4d24b01b8a0035a1f4b
narghev <n.ghevandiani@gmail.com>
2017-04-03 20:22:27 +0400

    wrapping entire app in muithemeprovoder

--- src/lib/App.jsx
+++ src/lib/App.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { Router, Route, hashHistory, Link } from 'react-router';
+
+const muiTheme = getMuiTheme({
+    tabs: {
+        backgroundColor: '#071112'
+    },
+    inkBar: {
+        backgroundColor: '#F8BD42'
+    }
+});
 
 import '../styles/frontComps.css';
 import '../styles/navButtons.css';
@@ -26,9 +37,11 @@ import FrontComps from './components/frontComps.jsx';
 class App extends React.Component {
    render() {
       return (
+        <MuiThemeProvider muiTheme={muiTheme}>
         <Router history={ hashHistory }>
           <Route path='/' component = { FrontComps }></Route>
         </Router>
+        </MuiThemeProvider>
       );
    }
 }
