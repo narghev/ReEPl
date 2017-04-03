ebfe752bd147b0dd03a76b7986cf78921b649d26
narghev <n.ghevandiani@gmail.com>
2017-04-03 20:59:32 +0400

    swipable views

--- src/lib/components/menu/menu.jsx
+++ src/lib/components/menu/menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
 import Playlist from '../playlist/playlist.jsx';
 import Options from './options.jsx';
 import Equilizer from './equilizer.jsx';
@@ -9,21 +10,22 @@ import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
-const muiTheme = getMuiTheme({
-    tabs: {
-        backgroundColor: '#071112'
-    },
-    inkBar: {
-        backgroundColor: '#F8BD42'
-    }
-})
-
 export default
 class Menu extends React.Component {
   constructor(){
     super();
-    this.state = {show: false};
+    this.state = {
+      show: false,
+      slideIndex: 0
+    };
   }
+
+  handleChange = (value) => {
+    this.setState({
+      slideIndex: value,
+    });
+  };
+
   render(){
      return (
        <div className="menu" style={(()=>{
@@ -31,30 +33,40 @@ class Menu extends React.Component {
            return {left: '0vw', transition: 'left 0.5s'};
          return {left: '-100vw', transition: 'left 0.5s'};
        })()}>
-         <div className="done" onClick={ this.props.hideMenu }>
-           <img src="images/done.svg" />
-         </div>
-         <MuiThemeProvider muiTheme={muiTheme}>
-           <Tabs className="tabs" initialSelectedIndex = { 0 } >
-             <Tab label = "Playlist">
-               <Playlist
-                 playlist={ this.props.playlist }
-                 clickHandler={ this.props.clickHandler }
-                 deleteClickHandler={ this.props.deleteClickHandler }
-               />
-             </Tab>
-             <Tab label = "Options">
-               <Options
-                 clearPlaylistClickHandler= { this.props.clearPlaylistClickHandler }
-                 changeAnimation= { this.props.changeAnimation }
-                 shuffle = { this.props.shuffle }
-                 replay = { this.props.replay }
-                 toggleShuffle = { this.props.toggleShuffle }
-                 toggleReplay = { this.props.toggleReplay }
-               />
-             </Tab>
-           </Tabs>
-         </MuiThemeProvider>
+           <div className="done" onClick={ this.props.hideMenu }>
+             <img src="images/done.svg" />
+           </div>
+          <Tabs
+           className="tabs"
+           initialSelectedIndex = { 0 }
+           onChange={this.handleChange}
+           value={this.state.slideIndex}
+           >
+             <Tab label = "Playlist" value={0} />
+             <Tab label = "Options" value={1} />
+          </Tabs>
+          <SwipeableViews
+            index={this.state.slideIndex}
+            onChangeIndex={this.handleChange}
+          >
+            <div>
+            <Playlist
+              playlist={ this.props.playlist }
+              clickHandler={ this.props.clickHandler }
+              deleteClickHandler={ this.props.deleteClickHandler }
+            />
+            </div>
+            <div>
+            <Options
+              clearPlaylistClickHandler= { this.props.clearPlaylistClickHandler }
+              changeAnimation= { this.props.changeAnimation }
+              shuffle = { this.props.shuffle }
+              replay = { this.props.replay }
+              toggleShuffle = { this.props.toggleShuffle }
+              toggleReplay = { this.props.toggleReplay }
+            />
+            </div>
+          </SwipeableViews>
        </div>
      )
   }
